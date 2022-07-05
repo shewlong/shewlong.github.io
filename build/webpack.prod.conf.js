98ba3c8cef8153a72a06b2809ecf8ac03901c532
刘小龙 <934307289@qq.com>
2022-07-05 15:24:57 +0800

    webpack 开启gzip压缩尝试

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -128,6 +128,7 @@ if (config.build.productionGzip) {
 
     webpackConfig.plugins.push(
         new CompressionWebpackPlugin({
+
             asset: '[path].gz[query]',
             algorithm: 'gzip',
             test: new RegExp(
